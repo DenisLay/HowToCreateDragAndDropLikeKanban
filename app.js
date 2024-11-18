352b81ce5eec486b5ea3526f7f829296e2c5af2f
Denis Kobzar <laycode00700@gmail.com>
2024-11-18 13:37:49 +0200

    items exchange

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ window.onload = () => {
     let leftBox = document.querySelector('.left-box');
     let rightBox = document.querySelector('.right-box');
 
-    console.log(items);
-
     for (item of items) {
         item.addEventListener('dragstart', (e) => {
             let selected = e.target;
@@ -13,8 +11,17 @@ window.onload = () => {
                 e.preventDefault();
             });
 
+            leftBox.addEventListener('dragover', (e) => {
+                e.preventDefault();
+            });
+
             rightBox.addEventListener('drop', (e) => {
                 rightBox.appendChild(selected);
+                selected = null;
+            });
+
+            leftBox.addEventListener('drop', (e) => {
+                leftBox.appendChild(selected);
                 selected = null;
             });
         });
